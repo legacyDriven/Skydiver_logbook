69d5dd54826f4d8e2d1480a9dd8c13d05d0b7c9b
Eugeniusz <eugenioskye@gmail.com>
2022-09-15 03:05:50 +0200

    Initial commit

---
+++ src/main/java/pl/skydive/logbook/skydiver_logbook/SkydiverLogbookApplication.java
@@ -0,0 +1,13 @@
+package pl.skydive.logbook.skydiver_logbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkydiverLogbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkydiverLogbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/skydive/logbook/skydiver_logbook/SkydiverLogbookApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.skydive.logbook.skydiver_logbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkydiverLogbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
